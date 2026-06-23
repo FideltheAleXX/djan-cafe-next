bf8fe9aee3ae5092d6cbea1cafbd715e8f3383ca
theAleXX <th3alexx@gmail.com>
2026-06-23 19:31:49 +0200

    fix errors

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next';
+import '../globals.css';
+
+export const metadata: Metadata = {
+  title: 'Про нас | Кафе "Джан"',
+};
+
+export default function AboutPage() {
+  return <div>Про нас</div>;
+}
---
+++ app/cart/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next';
+import '../globals.css';
+
+export const metadata: Metadata = {
+  title: 'Кошик | Кафе "Джан"',
+};
+
+export default function CartPage() {
+  return <div>Ця сторінка ще в розробці</div>;
+}
